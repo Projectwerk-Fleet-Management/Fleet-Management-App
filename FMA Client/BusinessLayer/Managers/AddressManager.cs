600d1e8be72f389e93f675319d9ea50a8520304e
protobear <43970498+protobear@users.noreply.github.com>
2021-12-13 15:14:29 +0100

    smoll update

---
+++ FMA Client/BusinessLayer/Managers/AddressManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLayer.Exceptions;
+using BusinessLayer.Interfaces;
+
+namespace BusinessLayer.Managers
+{
+    public class AddressManager
+    {
+
+        private IAddressRepository _repo;
+        public AddressManager(IAddressRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IReadOnlyList<Address> GetAllAddresses()
+        {
+            try
+            {
+                return _repo.GetAllAddresses();
+            }
+            catch (Exception e)
+            {
+                throw new AddressManagerException("failed to get all addresses", e);
+            }
+        }
+
+        public IReadOnlyList<Address> GetAddress(Address a)
+        {
+            try
+            {
+                return _repo.GetAddress(a.AddressId, a.Street, a.Housenumber, a.Addendum, a.City, a.Postalcode);
+            }
+            catch (Exception e)
+            {
+                throw new AddressManagerException("cannot get address", e);
+            }
+        }
+
+        public bool Exists(Address a)
+        {
+            try
+            {
+                return _repo.Exists(a.AddressId, a.Street, a.Housenumber, a.Addendum, a.City, a.Postalcode);
+            }
+            catch (Exception e)
+            {
+
+                throw new AddressManagerException("couldn't execute if exists", e);
+            }
+        }
+
+        public void Insert(Address a)
+        {
+            try
+            {
+                if(!Exists(a)) _repo.InsertAddress(a.Street, a.Housenumber, a.Addendum, a.City, a.Postalcode);
+            }
+            catch (Exception e)
+            {
+                throw new AddressManagerException("failed to insert", e);
+            }
+        }
+
+        public void Delete(Address a)
+        {
+            try
+            {
+                if(Exists(a)) _repo.DeleteAddress(a);
+            }
+            catch (Exception e)
+            {
+                throw new AddressManagerException("could not delete", e);
+            }
+        }
+
+        public void Update(Address oldAddress, Address newAddress)
+        {
+            try
+            {
+                if(Exists(oldAddress)) _repo.UpdateAddress(oldAddress, newAddress);
+            }
+            catch (Exception e)
+            {
+                throw new AddressManagerException("failed to update", e);
+            }
+        }
+
+
+
+    }
+}
--- FMA Client/Views/NewWindows/NewDriverWindow.xaml.cs
+++ FMA Client/Views/NewWindows/NewDriverWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BusinessLayer;
 using BusinessLayer.Model;
 
 namespace Views.NewWindows
@@ -46,13 +47,92 @@ namespace Views.NewWindows
             throw new NotImplementedException();
         }
 
-        private void createDriverLicenseList()
+        private List<LicenseType> createDriverLicenseList()
         {
             List<LicenseType> drlList = new List<LicenseType>();
+            if (AM.IsChecked == true)
+            {
+                drlList.Add(LicenseType.AM);
+            }
+
+            if (A.IsChecked == true)
+            {
+                drlList.Add(LicenseType.A);
+            }
+
             if (A1.IsChecked == true)
             {
                 drlList.Add(LicenseType.A1);
             }
+
+            if (A2.IsChecked == true)
+            {
+                drlList.Add(LicenseType.A2);
+            }
+
+            if (B.IsChecked == true)
+            {
+                drlList.Add(LicenseType.B);
+            }
+
+            if (BE.IsChecked == true)
+            {
+                drlList.Add(LicenseType.BE);
+            }
+
+            if (C.IsChecked == true)
+            {
+                drlList.Add(LicenseType.C);
+            }
+
+            if (CE.IsChecked == true)
+            {
+                drlList.Add(LicenseType.CE);
+            }
+
+            if (C1.IsChecked == true)
+            {
+                drlList.Add(LicenseType.C1);
+            }
+
+            if (C1E.IsChecked == true)
+            {
+                drlList.Add(LicenseType.C1E);
+            }
+
+            if (D.IsChecked == true)
+            {
+                drlList.Add(LicenseType.D);
+            }
+
+            if (DE.IsChecked == true)
+            {
+                drlList.Add(LicenseType.DE);
+            }
+
+            if (D1.IsChecked == true)
+            {
+                drlList.Add(LicenseType.D1);
+            }
+
+            if (D1E.IsChecked == true)
+            {
+                drlList.Add(LicenseType.D1E);
+            }
+
+            if (G.IsChecked == true)
+            {
+                drlList.Add(LicenseType.G);
+            }
+
+            return drlList;
+
+        }
+
+        private void createDriverAddress()
+        {
+            Address driverAddress = new Address(null, straatnaamField, housenumberField, addendumField, cityField,
+                postalcodeField, null);
         }
     }
 }
